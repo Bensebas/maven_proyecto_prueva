482ff613f8be1701e4e4627c337fad079955bc42
USUARIO <USUARIO@192.168.200.7>
2021-09-24 20:48:59 -0500

    MEET

--- scr/main/java3/prueba/consecionario/Main.java
+++ scr/main/java3/prueba/consecionario/Main.java
@@ -1,5 +1,6 @@
 package prueba.consecionario;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,11 +11,21 @@ public class Main {
 		Scanner LectorN = new Scanner(System.in);
 		int opcion = 0;
 		String ter = " ";
-		
-		AutoImportado autoimportado1 = new AutoImportado();
-		
-
-		Auto Datos[] = new Auto[5];
+		String marcaI = "";
+		String modeloI = "";
+		int añoI = 0;
+		String placaI = "";
+		int kilometrajeI = 0;
+		int codigoImportacion = 0;
+		int valorImportacion = 0;
+		String marcaN = "";
+		String modeloN = "";
+		int añoN = 0;
+		String placaN = "";
+		int kilometrajeN = 0;
+		int codigoNacionalN = 0;
+		AutoNacional DatosN[] = new AutoNacional[5];
+		AutoImportado DatosI[] = new AutoImportado[5];
 
 		do {
 			System.out.println("#################################");
@@ -32,13 +43,58 @@ public class Main {
 				System.out.println("a) Auto Nacional");
 				System.out.println("b) Auto Importado");
 				ter = Lector.nextLine();
+				
 
 				if (ter.equals("a")) {
 					System.out.println("Ingrese •Marca •Modelo •Año fabricación •Placa •Kilometraje •Código Nacional");
+					for (int i = 0; i < 5; i++) {
+
+						AutoNacional autonacional = new AutoNacional();
+						System.out.println("Ingrese datos del autonacional" + i);
+
+						marcaN = Lector.nextLine();
+						modeloN = Lector.nextLine();
+						añoN = LectorN.nextInt();
+						placaN = Lector.nextLine();
+						kilometrajeN = LectorN.nextInt();
+						codigoNacionalN = LectorN.nextInt();
+
+						autonacional.setMarca(marcaN);
+						autonacional.setModelo(modeloN);
+						autonacional.setAñoFabricacion(añoN);
+						autonacional.setPlaca(placaN);
+						autonacional.setKilometraje(kilometrajeN);
+						autonacional.setCodigoNacional(codigoNacionalN);
+						DatosN[i] = autonacional;
+
+					}
 
 				} else if (ter.equals("b")) {
 					System.out.println(
 							"Ingrese •Marca •Modelo •Año fabricación •Placa •Kilometraje •Código importación •Valor de la importación ");
+					for (int i = 0; i < 5; i++) {
+						AutoImportado autoimportado = new AutoImportado();
+
+						System.out.println("Ingrese datos del autonacional" + i);
+
+						marcaI = Lector.nextLine();
+						modeloI = Lector.nextLine();
+						añoI = LectorN.nextInt();
+						placaI = Lector.nextLine();
+						kilometrajeI = LectorN.nextInt();
+						codigoImportacion = LectorN.nextInt();
+						valorImportacion = LectorN.nextInt();
+
+						autoimportado.setMarca(marcaI);
+						autoimportado.setModelo(modeloI);
+						autoimportado.setAñoFabricacion(añoI);
+						autoimportado.setPlaca(placaI);
+						autoimportado.setKilometraje(kilometrajeI);
+						autoimportado.setCódigoImportación(codigoImportacion);
+						autoimportado.setValorDeLaImportacion(valorImportacion);
+						DatosI[i] = autoimportado;
+
+					}
 
 				}
 
@@ -46,6 +102,20 @@ public class Main {
 
 			case 2:
 				System.out.println("Ingrese el número de placa del Auto");
+				for (int B = 0; B < 5; B++) {
+					AutoNacional A [] = DatosN;
+					String no1 = placaI;
+					String no2 = Lector.nextLine();
+					boolean igual = no1.equals(no2);
+					System.out.println("yo" + Arrays.toString(DatosN));
+
+					if (igual == true) {
+						System.out.println("SI");
+
+					} else {
+						System.out.println("NO");
+					}
+				}
 
 				break;
 			case 3:
@@ -62,5 +132,4 @@ public class Main {
 		} while (opcion != 5);
 
 	}
-
 }
